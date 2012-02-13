bbe67df283d88f82a88ccf017843012d5adccc5a
FrancisVarga <github@varga-net.com>
2012-02-13 17:53:40 +0100

    wtf

---
+++ core/Abstracts/JsonRpc/Error.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 2/5/12
+ * Time: 10:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Abstracts\JsonRpc;
+class Error
+{
+
+}
