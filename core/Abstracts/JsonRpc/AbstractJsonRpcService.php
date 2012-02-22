18a7ab79b967f07dd855ceebe7a5f39e448ebaf1
FrancisVarga <github@varga-net.com>
2012-02-22 09:46:58 +0100

    add logging functionality

--- core/Abstracts/JsonRpc/AbstractJsonRpcService.php
+++ core/Abstracts/JsonRpc/AbstractJsonRpcService.php
@@ -1,14 +1,42 @@
 <?php
 
-/** 
+/**
  * @author fightbulc
- * 
- * 
+ *
+ *
  */
 namespace Processus\Abstracts\JsonRpc
 {
-	abstract class AbstractJsonRpcService extends \Processus\Abstracts\AbstractClass
+    abstract class AbstractJsonRpcService extends \Processus\Abstracts\AbstractClass
     {
+        /**
+         * @param $method
+         * @param $request
+         * @param $duration
+         * @param $metaData
+         */
+        protected function _logJsonRpc(\string $method, $request, $duration, $metaData)
+        {
+            $mysql = $this->getProcessusContext()->getMasterMySql();
+
+            $insertData = array(
+                "method"    => $method,
+                "request"   => json_encode($request),
+                "meta_data" => json_encode($metaData),
+                "duration"  => $duration,
+                "created"   => time(),
+            );
+
+            $mysql->insert($this->_getLogTransactionTable(), $insertData);
+        }
+
+        /**
+         * @return string
+         */
+        protected function _getLogTransactionTable()
+        {
+            return "log_json_rpc";
+        }
     }
 }
 
