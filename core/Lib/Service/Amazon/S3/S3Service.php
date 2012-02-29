50c693f96908c64790bbd1259af26b64f4bb2ed1
FrancisVarga <github@varga-net.com>
2012-02-29 12:42:13 +0100

    add some services stuff

--- core/Lib/Service/Amazon/S3/S3Service.php
+++ core/Lib/Service/Amazon/S3/S3Service.php
@@ -7,13 +7,80 @@
  * To change this template use File | Settings | File Templates.
  */
 namespace Processus\Lib\Service\Amazon\S3;
-class S3Service extends \Processus\Abstracts\AbstractClass
+class S3Service extends \Processus\Abstracts\AbstractClass implements \Processus\Interfaces\InterfaceDatabase
 {
+    /**
+     * @var \Zend\Service\Amazon\S3\S3
+     */
     private $_s3Service;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $amazonConfig     = $this->getProcessusContext()->getRegistry()->getProcessusConfig()->getAmazonConfig()->getS3Config()->getAuthData();
         $this->_s3Service = new \Zend\Service\Amazon\S3\S3($amazonConfig['aws_key'], $amazonConfig['secret']);
+    }
+
+    /**
+     * @return array|false
+     */
+    public function getBuckets()
+    {
+        return $this->_s3Service->getBuckets();
+    }
+
+    /**
+     * @param $objectString
+     *
+     * @return bool
+     */
+    public function remove($objectString)
+    {
+        return $this->_s3Service->removeObject($objectString);
+    }
+
+    public function fetch()
+    {
+        throw new \Processus\Exceptions\NotImplementedException();
+    }
+
+    /**
+     * @param null|string $objectString
+     *
+     * @return string|\Zend\Service\Amazon\S3\false
+     */
+    public function fetchOne(\string $objectString = null)
+    {
+        return $this->_s3Service->getObject($objectString);
+    }
+
+    /**
+     * @return array|false
+     */
+    public function fetchAll()
+    {
+        return $this->_s3Service->getBuckets();
+    }
+
+    /**
+     * @param null|string $sourcePath
+     * @param null        $object
+     * @param null        $metaInfo
+     *
+     * @return bool
+     */
+    public function insert(\string $sourcePath = null, $object = null, $metaInfo = null)
+    {
+        return $this->_s3Service->putFile($sourcePath, $object, $metaInfo);
+    }
+
+    /**
+     * @return \Zend\Db\Statement\Pdo
+     */
+    public function update()
+    {
+        throw new \Processus\Exceptions\NotImplementedException();
     }
 }
---
+++ core/Lib/Service/Twitter/TwitterService.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 2/29/12
+ * Time: 10:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Lib\Service\Twitter;
+class TwitterService extends \Processus\Abstracts\AbstractClass
+{
+    /**
+     * @var string
+     */
+    private $_accessToken;
+
+    /**
+     * @var string
+     */
+    private $_userName;
+
+    /**
+     * @var \Zend\Service\Twitter
+     */
+    private $_client;
+
+    /**
+     * @param $accessToken
+     *
+     * @return TwitterService
+     */
+    public function setAccessToken(\string $accessToken)
+    {
+        $this->_accessToken = $accessToken;
+        return $this;
+    }
+
+    /**
+     * @param $userName
+     *
+     * @return TwitterService
+     */
+    public function setUserName($userName)
+    {
+        $this->_userName = $userName;
+        return $this;
+    }
+
+    /**
+     * @return \Zend\Rest\Client\Result
+     * @throws TwitterServiceException
+     */
+    public function init()
+    {
+
+        if (is_null($this->_userName)) {
+            throw new TwitterServiceException("Username is not set!");
+        }
+
+        if (is_null($this->_accessToken)) {
+            throw new TwitterServiceException("Accesstoken is not set!");
+        }
+
+        $this->_client = new \Zend\Service\Twitter(array("username"    => $this->_userName,
+                                                         "accessToken" => $this->_accessToken
+                                                   ));
+        return $this;
+    }
+
+    /**
+     * @return \Zend\Rest\Client\Result
+     */
+    public function verify()
+    {
+        return $this->_client->accountVerifyCredentials();
+    }
+}
