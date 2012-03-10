11b0fd7cccfb1ffc179365a8bcf708ad8b540472
FrancisVarga <github@varga-net.com>
2012-03-10 12:50:11 +0100

    add AbstractJsonRpcServiceRequest wrapper

---
+++ core/Interfaces/InterfaceJsonRpcServiceRequest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 3/10/12
+ * Time: 12:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Interfaces;
+interface InterfaceJsonRpcServiceRequest extends InterfaceVo
+{
+
+}
