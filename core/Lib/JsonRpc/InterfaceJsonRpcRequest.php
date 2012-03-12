723b69f94fc0eb945e5aff2471c1b14b4ed84ed0
FrancisVarga <github@varga-net.com>
2012-03-12 13:05:51 +0100

    add JsonRpc lib into processus

---
+++ core/Lib/JsonRpc/InterfaceJsonRpcRequest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 3/12/12
+ * Time: 12:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Lib\JsonRpc;
+interface InterfaceJsonRpcRequest
+{
+    /**
+     * @abstract
+     * @param int $id
+     */
+    public function setRpcId(int $id);
+
+    /**
+     * @abstract
+     * @return int
+     */
+    public function getRpcId();
+
+    /**
+     * @abstract
+     * @param array $params
+     */
+    public function setParams(array $params);
+
+    /**
+     * @abstract
+     * @return array
+     */
+    public function getParams();
+
+    /**
+     * @abstract
+     * @param string $method
+     */
+    public function setMethod(string $method);
+
+    /**
+     * @abstract
+     * @return string
+     */
+    public function getMethod();
+
+    /**
+     * @abstract
+     * @return array
+     */
+    public function getPostData();
+}
